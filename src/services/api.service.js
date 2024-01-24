7e28659c85c056cade4c284459e97578dfa9c16b
gneris177 <gneris177@gmail.com>
2024-01-24 04:54:20 -0300

    update

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export class ApiService {
   // #baseUrl = 'https://meuapetite.com/api';
-  #baseUrl = 'http://127.0.1:3000/api';
+  #baseUrl = 'http://127.0.0.1:3000/api';
 
   getHeaders(multipart = false) {
     const headers = {};
