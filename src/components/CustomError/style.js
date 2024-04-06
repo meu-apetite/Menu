b7161ac3242923f3ba19b6656bd072a0c01e6fa6
gneris177 <gneris177@gmail.com>
2024-04-05 22:38:10 -0300

    fix: ajuste

--- src/components/CustomError/style.js
+++ src/components/CustomError/style.js
@@ -3,6 +3,9 @@ import { styled } from '@mui/system';
 export const FoundContainer = styled('div')`
   position: fixed;
   top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
   height: 100vh;
   width: 100vw;
   background: #fff;
